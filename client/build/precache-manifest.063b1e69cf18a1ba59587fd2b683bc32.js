05aa67297a94c154eba3bf33abc6f635ce7c9364
Ankit from mac <ankitbudhori23@gmail.com>
2024-12-08 21:20:44 +0530

    remove client build from .gitignore

---
+++ client/build/precache-manifest.063b1e69cf18a1ba59587fd2b683bc32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b3a727561b129740d5f0809ff2f368",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d700536a88f0626b503e",
+    "url": "/static/css/main.e081a844.chunk.css"
+  },
+  {
+    "revision": "8a17576ff35310a79558",
+    "url": "/static/js/2.ca14317e.chunk.js"
+  },
+  {
+    "revision": "bf52a793b6c79d77eb792bf9d7453a15",
+    "url": "/static/js/2.ca14317e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d700536a88f0626b503e",
+    "url": "/static/js/main.5f585c6d.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
